d6d6f31eb7fd382213048e20410d3e859530079d
danilo.xavier <danilo.xavier@corvostecnologia.com.br>
2018-12-19 08:07:50 -0200

    [PULL] - pull

--- invoices.php
+++ invoices.php
@@ -1,4 +1,4 @@
-<?php
+p<?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
